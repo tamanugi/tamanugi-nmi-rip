ce9be7edadc8f9ed54f63515030e53d768f44f4a
tamanugi <tamanugi@gmail.com>
2017-03-19 16:04:26 +0900

    fix trabis.yml and deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,37 +2,7 @@
 
 set -eu
 
-# hugo関係のファイルを置く場所。
-HUGO_DIR=./hugo
-echo "HUGO_DIR : ${HUGO_DIR}"
-
-# hugoのバイナリファイル
-HUGO_BIN="${HUGO_DIR}/hugo"
-echo "HUGO_BIN : ${HUGO_BIN}"
-
-# サイトを生成するディレクトリ
-DIST_DIR=./public
-echo "DIST_DIR : ${DIST_DIR}"
-
-# hugoのファイルがなかったらダウンロード
-if [ ! -e "${HUGO_DIR}" ];then
-  echo "Download hugo bin"
-
-  HUGO_PACKAGE=hugo.tgz
-  echo "HUGO_PACKAGE : ${HUGO_PACKAGE}"
-
-  echo "Create '${HUGO_DIR}'"
-  mkdir ${HUGO_DIR}
-
-  cd ${HUGO_DIR}
-  wget -O ${HUGO_PACKAGE} https://github.com/spf13/hugo/releases/download/v0.16/hugo_0.16_linux-64bit.tgz
-  tar -xvf ${HUGO_PACKAGE}
-  cd ..
-fi
-
-# サイトを生成
-echo "Compile site"
-${HUGO_BIN}
+DIST_DIR=public
 
 # 変更があったらcommit
 cd ${DIST_DIR}
